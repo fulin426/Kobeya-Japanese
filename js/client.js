7c20598ea58106efe3ed419936fe25d5d10e780c
fulin426 <liufam@ms-MacBook-Pro.local>
2019-02-08 23:01:21 -0800

    show two pictures for mobile in carousal

--- js/client.js
+++ js/client.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 2,
         infinite: true,
         dots: true
       }
@@ -60,7 +60,7 @@ $(document).ready(function(){
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1
       }
     }
