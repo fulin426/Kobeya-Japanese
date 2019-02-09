8bb8b88cab828812a56725435f729bdee874d4a0
fulin426 <liufam@ms-MacBook-Pro.local>
2019-02-08 22:27:04 -0800

    scroll feature, google maps api, cavier logo

--- js/client.js
+++ js/client.js
@@ -1,3 +1,37 @@
+//scroll feature
+//History
+$('.history-link').on('click', function(event) {
+	event.preventDefault();
+	let jump = $(this).attr("href");
+	let new_position = $(jump).offset();;
+	$('html, body').stop().animate({ scrollTop: new_position.top - 20 }, 600);
+});
+
+//menu
+$('.menu-link').on('click', function(event) {
+	event.preventDefault();
+	let jump = $(this).attr("href");
+	let new_position = $(jump).offset();;
+	$('html, body').stop().animate({ scrollTop: new_position.top - 15 }, 600);
+});
+
+//Find us
+$('.find-us-link').on('click', function(event) {
+	event.preventDefault();
+	let jump = $(this).attr("href");
+    console.log(jump);
+	let new_position = $(jump).offset();;
+	$('html, body').stop().animate({ scrollTop: new_position.top - 50 }, 600);
+});
+
+//scroll to top
+$('.top-trigger').on('click', function(event) {
+	event.preventDefault();
+	let jump = $(this).attr("href");
+	let new_position = $(jump).offset();;
+	$('html, body').stop().animate({ scrollTop: new_position.top - 50 }, 600);
+});
+
 //enable slick img carousel
 $(document).ready(function(){
   $('.carousel').slick({
@@ -36,3 +70,17 @@ $(document).ready(function(){
   ]
   });
 });
+//Google Maps
+function initMap() {
+  let options = {
+    zoom: 16,
+    center: {lat: 37.5442391, lng: -122.3059977}
+  };
+
+  let map = new google.maps.Map(document.getElementById('map'), options);
+
+  let marker = new google.maps.Marker({
+    position: {lat: 37.543630, lng: -122.307940},
+    map:map
+  })
+}
